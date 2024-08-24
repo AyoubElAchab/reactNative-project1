c2869f6e7a83cd4053d802ef6b2e2daa8c1f15a5
ayoub el achab <elachabayoubdev@gmail.com>
2024-08-24 13:26:33 +0100

    Capitalize

--- src/components/ExerciseFlatList.jsx
+++ src/components/ExerciseFlatList.jsx
@@ -8,7 +8,8 @@ export default function ExerciseFlatList ({item,index}){
       <View style={styles.exerciseContainer}>
         <Text style={styles.exerciseName}>{item.name} </Text>
         <Text style={styles.exerciseSubtitle}>
-          {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+          <Text style={styles.subValue}>{item.muscle}</Text> |
+          <Text style={styles.subValue}>{item.equipment}</Text>
         </Text>
       </View>
     )
@@ -30,5 +31,8 @@ export default function ExerciseFlatList ({item,index}){
     exerciseSubtitle : {
       color:'dimgray',
     },
+    subValue : {
+      textTransform :"capitalize",
+    }
   });
   
